b750ad6d249c5836379f720c34f32144ae538fbe
Hermmann Berbary <hberbary@berbsteam.com>
2022-05-16 11:40:11 -0300

    added page-content css id

--- components/Page/index.js
+++ components/Page/index.js
@@ -1,21 +1,26 @@
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
-export default function Page({ pageHeader, pageSidebar, children, className = '', fixed = false, widthLimiter = false }) {
-
-    return (<div className={`page ${fixed ? 'tbox-fixed' : ''} ${widthLimiter ? 'width-limiter' : ''} ${className}`}>
-        {pageHeader && <div className="page-header">
-            {pageHeader}
-        </div>}
-        <div className="page-content">
-            {pageSidebar && <div className="page-sidebar">
-                {pageSidebar}
-            </div>}
-            <div className="page-body">
-                {children}
-            </div>
-        </div>
-        <Box display="flex" justifyContent="space-between">
-        </Box>
-    </div>)
-
+export default function Page({
+  pageHeader,
+  pageSidebar,
+  children,
+  pageContentCssId,
+  className = "",
+  fixed = false,
+  widthLimiter = false,
+}) {
+  return (
+    <div
+      className={`page ${fixed ? "tbox-fixed" : ""} ${
+        widthLimiter ? "width-limiter" : ""
+      } ${className}`}
+    >
+      {pageHeader && <div className="page-header">{pageHeader}</div>}
+      <div className="page-content" id={pageContentCssId}>
+        {pageSidebar && <div className="page-sidebar">{pageSidebar}</div>}
+        <div className="page-body">{children}</div>
+      </div>
+      <Box display="flex" justifyContent="space-between"></Box>
+    </div>
+  );
 }
