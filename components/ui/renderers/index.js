57b8a929dcc20d108123c7cb85bf4c6dfec8c5f6
Hermmann Berbary <h.berbary@gmail.com>
2022-10-20 08:43:04 -0300

    added NullableBoolean renderer

--- components/ui/renderers/index.js
+++ components/ui/renderers/index.js
@@ -1,36 +1,43 @@
-import dayjs from "dayjs";
-import NumberFormat from "react-number-format";
+import dayjs from 'dayjs';
+import NumberFormat from 'react-number-format';
 
 export function Date({ value, format = 'DD/MM/YYYY' }) {
-    return dayjs(value).format(format);
+  return dayjs(value).format(format);
 }
 
 export function Float({ value, ...rest }) {
-    return (<NumberFormat
-        displayType={'text'}
-        value={value}
-        decimalSeparator=","
-        thousandSeparator="."
-        fixedDecimalScale={true}
-        decimalScale={2}
-        {...rest}
-    />)
+  return (
+    <NumberFormat
+      displayType={'text'}
+      value={value}
+      decimalSeparator=","
+      thousandSeparator="."
+      fixedDecimalScale={true}
+      decimalScale={2}
+      {...rest}
+    />
+  );
 }
 
 export function Money({ value, ...rest }) {
-    return (
-      <NumberFormat
-        displayType={'text'}
-        value={value}
-        decimalSeparator=","
-        thousandSeparator="."
-        fixedDecimalScale={true}
-        decimalScale={2}
-        prefix="R$"
-        {...rest}
-      />)
-  }
+  return (
+    <NumberFormat
+      displayType={'text'}
+      value={value}
+      decimalSeparator=","
+      thousandSeparator="."
+      fixedDecimalScale={true}
+      decimalScale={2}
+      prefix="R$"
+      {...rest}
+    />
+  );
+}
 
-  export function ID({ value }) {
-      return (<>#{value}</>);
-  }
+export function ID({ value }) {
+  return <>#{value}</>;
+}
+
+export function NullableBoolean({ value }) {
+  return <>{value ? 'Sim' : 'Não'}</>;
+}
