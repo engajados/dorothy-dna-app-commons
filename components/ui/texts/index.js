dbd59b0d5aeea22ac19eea4ac85bffdd5d9b9f6e
Hermmann Berbary <h.berbary@gmail.com>
2023-01-27 12:39:56 -0300

    shadows and radius updated

---
+++ components/ui/texts/index.js
@@ -0,0 +1,11 @@
+import styles from './texts.module.scss';
+
+export const text1 = () => {
+  return <></>;
+};
+export const text2 = () => {
+  return <></>;
+};
+export const text3 = () => {
+  return <></>;
+};
---
+++ components/ui/titles/index.js
@@ -0,0 +1,20 @@
+import styles from './titles.module.scss';
+
+export const title1 = () => {
+  return <></>;
+};
+export const title2 = () => {
+  return <></>;
+};
+export const title3 = () => {
+  return <></>;
+};
+export const title4 = () => {
+  return <></>;
+};
+export const title5 = () => {
+  return <></>;
+};
+export const title6 = () => {
+  return <></>;
+};
