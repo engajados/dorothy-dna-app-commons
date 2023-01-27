7ee5b4129a8fbe0881879d0e9c6d4abafbba4628
Hermmann Berbary <h.berbary@gmail.com>
2023-01-27 17:15:51 -0300

    shadows and radius updated pt2

--- components/ui/texts/index.js
+++ components/ui/texts/index.js
@@ -1,11 +1,13 @@
 import styles from './texts.module.scss';
 
-export const text1 = () => {
-  return <></>;
+export const text1 = ({ children }) => {
+  return <h1 className={styles.text1}>{children}</h1>;
 };
-export const text2 = () => {
-  return <></>;
+
+export const text2 = ({ children }) => {
+  return <h2 className={styles.text2}>{children}</h2>;
 };
-export const text3 = () => {
-  return <></>;
+
+export const text3 = ({ children }) => {
+  return <h3 className={styles.text3}>{children}</h3>;
 };
--- components/ui/titles/index.js
+++ components/ui/titles/index.js
@@ -1,20 +1,25 @@
 import styles from './titles.module.scss';
 
-export const title1 = () => {
-  return <></>;
+export const title1 = ({ children }) => {
+  return <h1 className={styles.title1}>{children}</h1>;
 };
-export const title2 = () => {
-  return <></>;
+
+export const title2 = ({ children }) => {
+  return <h2 className={styles.title2}>{children}</h2>;
 };
-export const title3 = () => {
-  return <></>;
+
+export const title3 = ({ children }) => {
+  return <h3 className={styles.title3}>{children}</h3>;
 };
-export const title4 = () => {
-  return <></>;
+
+export const title4 = ({ children }) => {
+  return <h4 className={styles.title4}>{children}</h4>;
 };
-export const title5 = () => {
-  return <></>;
+
+export const title5 = ({ children }) => {
+  return <h5 className={styles.title5}>{children}</h5>;
 };
-export const title6 = () => {
-  return <></>;
+
+export const title6 = ({ children }) => {
+  return <h6 className={styles.title6}>{children}</h6>;
 };
