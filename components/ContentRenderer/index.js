6a92386ff3ec182dc2c6725cab18a6280c82e8eb
Hermmann Berbary <h.berbary@gmail.com>
2023-03-28 11:43:08 -0300

    delete content_renderer module (ck style applied instead); applied sanitize

--- components/ContentRenderer/index.js
+++ components/ContentRenderer/index.js
@@ -1,21 +1,64 @@
-export default function ContentRenderer({ linkOrPage }) {
+import { useEffect, useState } from 'react';
+import SanitizedHTML from 'react-sanitized-html';
+
+import './ck-content-styles.css';
+
+export default function ContentRenderer({ type, contentId, text }) {
+  const [dirtyHtml, _dirtyHtml] = useState(null);
+
+  useEffect(() => {
+    if (!text)
+      fetch(`/content/${contentId}`)
+        .then(res => res.json())
+        .then(data => _dirtyHtml(data.text));
+    else _dirtyHtml(text);
+  }, [contentId, text]);
+
   return (
     <>
-      <h1>Hello, world!</h1>
-      <p>I`m a content renderer.</p>
-
-      <p>
-        at the current moment I only have this: <code>{linkOrPage}</code>
-      </p>
-
-      <h2>TODO:</h2>
-
-      <ul>
-        <li>
-          read the endpoint at <code>/content</code> and
-        </li>
-        <li>apply html sanitize in me. </li>
-      </ul>
+      <SanitizedHTML
+        allowedTags={[
+          'p',
+          'a',
+          'ul',
+          'li',
+          'ol',
+          'br',
+          'strong',
+          'em',
+          'u',
+          'i',
+          'h1',
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+          'img',
+          'table',
+          'thead',
+          'tbody',
+          'tr',
+          'th',
+          'td',
+          'figure',
+          'col',
+          'colgroup',
+        ]}
+        allowedAttributes={{
+          p: ['style'],
+          img: ['src'],
+          a: ['href'],
+          table: ['class'],
+          figure: ['class', 'style'],
+          col: ['style'],
+          td: ['class'],
+          th: ['class'],
+          tr: ['class'],
+        }}
+        html={dirtyHtml}
+        className={'ck-content'}
+      />
     </>
   );
 }
