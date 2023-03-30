ab0dd1c013f0e8c11a573abf717c898c1bb0d17c
Hermmann Berbary <h.berbary@gmail.com>
2023-03-30 09:03:24 -0300

    removed react-sanitize-html using only sanitize-html pkg

--- components/ContentRenderer/index.js
+++ components/ContentRenderer/index.js
@@ -1,53 +1,51 @@
-import SanitizedHTML from 'react-sanitized-html';
-
 import './ck-content-styles.css';
+import styles from './content_renderer.module.scss';
+import sanitize from 'sanitize-html';
 
-export default function ContentRenderer({ type, text }) {
+export default function ContentRenderer({ type, selectedImageUrl, text }) {
+  const sanitizedHtml = sanitize(text, {
+    allowedTags: [
+      'p',
+      'a',
+      'ul',
+      'li',
+      'ol',
+      'br',
+      'strong',
+      'em',
+      'u',
+      'i',
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'img',
+      'table',
+      'thead',
+      'tbody',
+      'tr',
+      'th',
+      'td',
+      'figure',
+      'col',
+      'colgroup',
+    ],
+    allowedAttributes: {
+      img: ['src'],
+      a: ['href'],
+      '*': ['style', 'class'],
+    },
+    parseStyleAttributes: false,
+  });
+
   return (
-    <>
-      <SanitizedHTML
-        allowedTags={[
-          'p',
-          'a',
-          'ul',
-          'li',
-          'ol',
-          'br',
-          'strong',
-          'em',
-          'u',
-          'i',
-          'h1',
-          'h2',
-          'h3',
-          'h4',
-          'h5',
-          'h6',
-          'img',
-          'table',
-          'thead',
-          'tbody',
-          'tr',
-          'th',
-          'td',
-          'figure',
-          'col',
-          'colgroup',
-        ]}
-        allowedAttributes={{
-          p: ['style'],
-          img: ['src'],
-          a: ['href'],
-          table: ['class'],
-          figure: ['class', 'style'],
-          col: ['style'],
-          td: ['class'],
-          th: ['class'],
-          tr: ['class'],
-        }}
-        html={text}
-        className={'ck-content'}
-      />
-    </>
+    <div className={'app-container'}>
+      <div className={styles.content_wrapper}>
+        {type === 'news' && <>{selectedImageUrl}</>}
+        <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} className={'ck-content'} />
+      </div>
+    </div>
   );
 }
