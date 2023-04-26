1611776fce55a15a4742a35f90076b7cfe43064f
Hermmann Berbary <h.berbary@gmail.com>
2023-04-26 10:01:21 -0300

    warning fix

--- components/Overlay/Modal/index.js
+++ components/Overlay/Modal/index.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import Text2 from "../../Typography/Texts/Text2";
+import Text2 from '../../Typography/Texts/Text2';
 import styles from './styles.module.scss';
 import { ReactComponent as Close } from '../../../icons/close.svg';
 import Button from '../../Actions/Button';
 import { isEmpty } from 'lodash';
 
-export default function Modal({ children, size, title, actionlabel, sidebar, contentpadding, modalState, onModalChange }) {
-
+export default function Modal({
+  children,
+  size,
+  title,
+  actionlabel,
+  sidebar,
+  contentpadding,
+  modalState,
+  onModalChange,
+}) {
   const [state, setModalState] = useState(modalState);
 
   useEffect(() => {
@@ -15,24 +23,27 @@ export default function Modal({ children, size, title, actionlabel, sidebar, con
 
   useEffect(() => {
     onModalChange(state);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [state]);
 
   const handleCloseModal = () => {
     setModalState('hide');
   };
-  
+
   return (
     <div className={`${styles.modal} ${styles[state]}`}>
       <div className={`${styles.backdrop}`} onClick={() => handleCloseModal()}></div>
       <div className={`${styles.dialog}  ${styles[size]}`}>
         <div className={`${styles.header}`}>
-          <div className={`${styles.title}`}><Text2>{title}</Text2></div>
-          <div className={`${styles.close}`} onClick={() => handleCloseModal()}><Close/></div>
+          <div className={`${styles.title}`}>
+            <Text2>{title}</Text2>
+          </div>
+          <div className={`${styles.close}`} onClick={() => handleCloseModal()}>
+            <Close />
+          </div>
         </div>
         <div className={`${styles.contentwrapper} ${styles[contentpadding]}`}>
-          {!isEmpty(sidebar) && (
-            <div className={`${styles.sidebar}`}>{sidebar}</div>
-          )}
+          {!isEmpty(sidebar) && <div className={`${styles.sidebar}`}>{sidebar}</div>}
           <div className={`${styles.body}`}>{children}</div>
         </div>
         <div className={`${styles.footer}`}>
@@ -41,7 +52,9 @@ export default function Modal({ children, size, title, actionlabel, sidebar, con
             <div className={`${styles.addanother}`}>
               <Button variant="link">Adicionar outra</Button>
             </div>
-            <Button variant="link" onClick={() => handleCloseModal()}>Cancelar</Button>
+            <Button variant="link" onClick={() => handleCloseModal()}>
+              Cancelar
+            </Button>
             <Button variant="primary">{actionlabel}</Button>
           </div>
         </div>
